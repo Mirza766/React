bfa07172631d379cba68dbda7aa28b0e92fa358a
Mirza766 <saifmirza766@gmail.com.in>
2025-04-12 20:42:25 +0500

    project setup by vite and react app

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+
+import Chai from "./chai";
+
+function App() {
+  return (
+    <>
+    <Chai/>
+      <h1>This is React series</h1>
+    </>
+ 
+  );
+}
+
+export default App;
---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "./chai"
+
+function App() {
+
+
+  return (
+    <>
+    <h1>React project using vite | Mirza Saif Ullah Baig</h1>
+  
+  <Chai/>
+  </>
+  )
+}
+
+export default App
