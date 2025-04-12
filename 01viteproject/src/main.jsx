f20cf24408865fc1d79e88d5fa40c30f082ccfd5
Mirza766 <saifmirza766@gmail.com.in>
2025-04-12 23:16:32 +0500

    updation

--- 01viteproject/src/main.jsx
+++ 01viteproject/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import React from 'react';
 import { createRoot } from 'react-dom/client'
-import {jsx as _jsx} from "react/jsx-dev-runtime.js"
+//import {jsx as _jsx} from "react/jsx-dev-runtime.js"
 import App from './App.jsx'
 
 
